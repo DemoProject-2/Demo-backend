8407affd0a693dc61110bd33c18a283698eec9c9
Nila <Nila@LAPTOP-M0QERO78>
2021-05-07 08:58:17 -0400

    added routes folder

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const db = require('../db')
+const verifyToken = require('../middleware/verifytoken')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+router.post('/sign-in', async function (req,res){
+    try{
+        let username = req.body.username
+        const password = req.body.password
+        const data = await db.any(`SELECT users.id, users.account_type, users.password FROM users WHERE users.username = '${username}'`)
+        const samePassword = bcrypt.compareSync(password,data[0].password)
+        if(samePassword){
+            jwt.sign({data},process.env.RANDOM_TOKEN, { expiresIn : '3600s'}, async (err, token) =>{
+                await res.status(202).json({
+                    id:data[0].id,
+                    account_type: data[0].account_type,
+                    isAuthorized: true,
+                    username: data[0].username,
+                    token
+                })
+            })
+        } else {
+            res.status(403).json({
+                isAuthorized:false
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            error: err,
+            isAuthorized:false
+        })
+    }
+})
