fb301a8204f0393965c387b379e24355873c6d91
imt1195 <imt1195@gmail.com>
2021-05-15 12:44:16 -0400

    knex migrations and seed files updates

--- seeds/seed_specialties.js
+++ seeds/seed_specialties.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('disorders').del()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {disorder_id: 1, colName: 'Depression'},
+        {disorder_id: 2, colName: 'Anxiety'},
+        {disorder_id: 3, colName: 'Substance Abuse'}
       ]);
     });
 };
