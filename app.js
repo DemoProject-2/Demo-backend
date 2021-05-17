4accdfca3eefb35eefa52a4d0e72af5687d13222
imt1195 <imt1195@gmail.com>
2021-05-17 10:53:13 -0400

    cors added

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 // app.use('/', indexRouter);
 app.use('/users', usersRouter);
