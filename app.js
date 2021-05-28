9d058cf04be0085fc97c4d240f55dcc2695b9654
imt1195 <imt1195@gmail.com>
2021-05-27 22:00:04 -0400

    small changes for connections

--- app.js
+++ app.js
@@ -17,13 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
 
 app.use('/', authRouter);
 app.use('/users', usersRouter);
